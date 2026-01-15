1c5d036dc96cd960676b245bad5c798e2baf5cb8
Dominic Chege <dominicchege125796@gmail.com>
2026-01-15 12:06:19 +0300

    Add files via upload

---
+++ background_color.js
@@ -0,0 +1,9 @@
+document.getElementById("skyblue").addEventListener('click' , () => {
+            document.body.style.backgroundColor = "skyblue"
+            
+        })
+        const results = document.getElementById("result")
+        document.getElementById("user").addEventListener('click' , () => {
+            document.body.style.backgroundColor = "lightgrey"
+            
+        })
